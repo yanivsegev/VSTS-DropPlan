fd255a074ea692caf941f9db0ab4ac41d26f5f72
Ian Wilkinson <Ian.Wilkinson@theaccessgroup.com>
2024-01-07 19:35:18 +0000

    fix issues with UTF8 chars being mangled by DevOps unless the file has the BOM.

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -7,6 +7,7 @@ import concat from 'gulp-concat';
 import sourcemaps from 'gulp-sourcemaps';
 import fs from 'fs'
 import log from 'fancy-log'
+import bom from 'gulp-bom'
 
 import mergeStream from 'merge-stream';
 
@@ -29,6 +30,10 @@ let css = {
         dev:{
             path: './dist/dev/Styles/',
             extension: '.css'
+        },
+        qa:{
+            path: './dist/qa/Styles/',
+            extension: '.min.css'
         },
         prod:{
             path: './dist/prod/Styles/',
@@ -74,6 +79,14 @@ let js = {
             path: './dist/dev/scripts/',
             extension: '.js'
         },
+        qa: {
+            path: './dist/qa/scripts/',
+            extension: '.min.js',
+            sourceFiles: [
+                /*"scripts/ga.js",
+                "scripts/trackjs.js"*/
+            ]
+        },
         prod: {
             path: './dist/prod/scripts/',
             extension: '.min.js',
@@ -117,6 +130,7 @@ let Production = {
                 .pipe(sourcemaps.init({loadMaps: true}))
                 //.pipe(uglify())
                 .pipe(sourcemaps.write('.', {addComment: false}))
+                .pipe(bom())
                 .pipe(gulp.dest(js.environment.prod.path))
             })
         )
@@ -129,25 +143,49 @@ let Production = {
     Env: 'prod'
 };
 
+let QA = {
+    Scripts: function(){
+        return mergeStream(js.outputFiles.map((source)=>{
+            return gulp.src(js.environment.qa.sourceFiles.concat([js.environment.dev.path + source.fileName + js.environment.dev.extension]))
+                .pipe(concat(source.fileName + js.environment.qa.extension))
+                .pipe(sourcemaps.init({loadMaps: true}))
+                //.pipe(uglify())
+                .pipe(sourcemaps.write('.', {addComment: false}))
+                .pipe(bom())
+                .pipe(gulp.dest(js.environment.qa.path))
+            })
+        )
+    },
+    Styles: function(){
+        return gulp.src([css.environment.dev.path + css.fileName + css.environment.dev.extension])
+          .pipe(rename({extname: css.environment.qa.extension}))
+          .pipe(gulp.dest(css.environment.qa.path));
+    },
+    Env: 'qa'
+};
+
 function clean(){
     return del('dist');
 }
 
 function loadBuildVersion(){
-    let buildVersion=0;
-    try{
-        buildVersion = parseInt(fs.readFileSync('buildVersion', 'utf8'))+1;
-        log('Build version set to "' + buildVersion + '"');
-    }catch(e){
-        log('unable to load build version'+e);
+    if(!loadBuildVersion.buildVersion){
+        let buildVersion=0;
+        try{
+            buildVersion = parseInt(fs.readFileSync('buildVersion', 'utf8'))+1;
+            log('Build version set to "' + buildVersion + '"');
+        }catch(e){
+            log('unable to load build version'+e);
+        }
+        try{
+            fs.writeFileSync('buildVersion', ''+buildVersion);
+            log('Build version written to disk');
+        }catch(e){
+            log('unable to save build version' + e);
+        }
+        loadBuildVersion.buildVersion=buildVersion
     }
-    try{
-        fs.writeFileSync('buildVersion', ''+buildVersion);
-        log('Build version written to disk');
-    }catch(e){
-        log('unable to save build version' + e);
-    }
-    return buildVersion
+    return loadBuildVersion.buildVersion
 }
 
 function copyStaticFiles(env){
@@ -220,7 +258,10 @@ let build = gulp.series(
         Development.Scripts,
         copyNodeScripts(Development.Env),
         copyStaticFiles(Development.Env),
+        copyNodeScripts(Production.Env),
         copyStaticFiles(Production.Env),
+        copyNodeScripts(QA.Env),
+        copyStaticFiles(QA.Env),
         copyDynamicFiles(Development.Env, [
             {Key: '#{now}', Value: new Date().toJSON()},
             {Key: '#{testing-flag}', Value: '-test'},
@@ -230,6 +271,15 @@ let build = gulp.series(
             {Key: '"uri": "index.html"', Value: '"uri": "https://localhost:8080"'},
             {Key: '"uri": "dropPlan-settings.html"', Value: '"uri": "https://localhost:8080/dropPlan-settings.html"'},
             {Key: '#{isMinified}', Value: ''}
+        ]),
+        copyDynamicFiles(QA.Env, [
+            {Key: '#{now}', Value: new Date().toJSON()},
+            {Key: '#{testing-flag}', Value: '-test'},
+            {Key: '#{testing-flag}', Value: ''},
+            {Key: '#{beta-flag}', Value: '.'+loadBuildVersion()},
+            {Key: '"public": false', Value: '"public": false'},
+            {Key: '"yanivsegev"', Value: '"' + publisherId + '"'},
+            {Key: '#{isMinified}', Value: '.min'}
         ]),
         copyDynamicFiles(Production.Env, [
             {Key: '#{now}', Value: new Date().toJSON()},
@@ -242,6 +292,8 @@ let build = gulp.series(
         ])
     ),
     gulp.parallel(
+        QA.Scripts,
+        QA.Styles,
         Production.Scripts,
         Production.Styles
     )
